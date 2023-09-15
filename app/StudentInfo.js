2341ec55678429235a80e3ce06dfa7f8a98371a4
mjmorelos <markjoseph.morelos@edu.sait.ca>
2023-09-15 11:37:39 -0600

    week2

---
+++ app/StudentInfo.js
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+
+export default function StudentInfo(){
+    return(
+        <main>
+        <p>
+            Mark Joseph Morelos
+        </p>
+        <p>
+            CPRG 306 C
+        </p>
+        <p>
+            <a href="https://github.com/mjmorelos/cprg306-assignments" target="_blank">Github account</a>
+        </p>
+        </main>
+    )
+}
