5127f9817b57f3b77d7c08a2831023d952e6e259
mjmorelos <markjoseph.morelos@edu.sait.ca>
2023-10-09 23:29:20 -0600

    week4-updated

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -7,26 +7,40 @@ export default function NewItem() {
     const [quantity, setQuantity] = useState(1);
     const [category, setCategory] = useState("produce");
 
-    const handleSubmit = (item) => {
-        item.preventDefault();
+    function handleSubmit(submit)
+    {
+        submit.preventDefault();
         const newItem = { name, quantity, category };
         console.log(newItem);
         alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
         setName("");
         setQuantity(1);
         setCategory("produce");
-    };
+    }
+    function handleNameChange(change)
+    {
+        setName(change.target.value);
+    }
+    function handleQuantityChange(change)
+    {
+        setQuantity(change.target.value);
+    }
+    function handleCategoryChange(change)
+    {
+        setCategory(change.target.value);
+    }
+
 
     return(
         <main>
             <div className="flex items-center justify-center">
                 <div>
                     <form onSubmit={handleSubmit}>
-                        <input required onChange={setName} value={name} placeholder="Item name" />
+                        <input required onChange={handleNameChange} value={name} placeholder="Item name" />
                         <br /> <br />
-                        <input required onChange={setQuantity} value={quantity} type="number" min={1} max={99}/>
+                        <input required onChange={handleQuantityChange} value={quantity} type="number" min={1} max={99}/>
                         <br /> <br />
-                        <select required onChange={setCategory} value={category}>
+                        <select required onChange={handleCategoryChange} value={category}>
                         <option value="produce">Produce</option>
                         <option value="dairy">Dairy</option>
                         <option value="bakery">Bakery</option>
@@ -46,6 +60,6 @@ export default function NewItem() {
             </div> 
         </main>
     )
-}
+};
             
                 
